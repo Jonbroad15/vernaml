b0a1a25970c966c404c2dce60714faad245a6718
Jonbroad15 <jonbroad15@gmail.com>
2020-10-16 13:11:27 -0400

    add options to subset on any or a list of rna, protein, ligand

--- scripts/subset_graphs.py
+++ scripts/subset_graphs.py
@@ -3,31 +3,35 @@ import argparse
 import os
 import pandas as pd
 import csv
-from subset_graphs import *
+from comp402.subset_graphs import slice_graph
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', help='input directory', default ='../data/unchopped_v4_nr')
     parser.add_argument('subset', help='CSV file of subset of interface residues')
     parser.add_argument('output_dir', help='output directory')
-    parser.add_argument('-t', help='interaction type = {protein, ion, rna, ligand, all}', default='all')
+    parser.add_argument('-t','--type',
+                        help='interaction type = {protein, ion, rna, ligand, all}',
+                        default='all')
     args = parser.parse_args()
 
     input_dir = args.i
     output_dir = args.output_dir
     try:
-        output_prime_dir = os.mkdir(f'{output_dir}complement')
+        os.mkdir(f'{output_dir}complement')
     except FileExistsError:
-        output_prime_dir = f'{output_dir}complement'
-    interaction_type = args.t
+        print('complement directory already exists! make sure you are not overwriting')
+    output_prime_dir = f'{output_dir}complement'
+    interaction_type = args.type.split(' ')
 
     # Initialize subset from subset csv input argument
     subset = {}
+    print('Building interface subset dictionary...')
     with open(args.subset, 'r') as f:
         reader = csv.reader(f, delimiter=',')
         for items in reader:
-            if items[3] != interaction_type and interaction_type != 'all': continue
-            if items[1] == 'position': continue
+            if items[3] not in interaction_type and 'all' not in interaction_type: continue
+            if items[1] == 'position' or items[1] == 'postion': continue
             pbid = f'{items[0]}.nx'
             if pbid in subset.keys():
                 subset[pbid].append((pbid, (items[2], int(items[1]))))
@@ -36,6 +40,7 @@ def main():
 
     # slice graphs
     for f in os.listdir(input_dir):
+        print(f'Slicing RNA graph: \t {f}')
         graph_file = os.path.join(input_dir, f)
         g = nx.read_gpickle(os.path.join(input_dir, f))
         pbid = list(g.nodes)[0][0]
