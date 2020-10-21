d2db30a6ce80a620647bc7153e43b925e40f7cbc
Jonbroad15 <jonbroad15@gmail.com>
2020-10-21 17:06:03 -0400

    Introduce new testing framework for is_dna

--- prepare_data/tests/is_dna.py
+++ prepare_data/tests/is_dna.py
@@ -1,83 +1,52 @@
 import unittest
 from Bio.PDB import *
-from ..is_dna import *
+from ..interfaces import *
 import os
+
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+rnadir = os.path.join(scriptdir, '..', '..',
+                        'data', 'structures')
+test_structures_dir = os.path.join(scriptdir, '..', '..',
+                        'data', 'test_structures')
+ligands_file = os.path.join(scriptdir, '..', '..',
+                                'data', 'ligand_list.txt')
+ligands = []
+with open(ligands_file, 'r') as f:
+    for line in f.readlines():
+        ligands.append(line.strip())
+
+parser = MMCIFParser(QUIET=True)
+
+
 
 class is_dnaTestCase(unittest.TestCase):
 
     def test_is_dna(self):
 
-        parser = MMCIFParser(QUIET=True)
+        filtered_residues = {}
+        unfiltered_residues = {}
+        for int_type in ['rna', 'protein', 'dna']:
 
-        dna_files = os.listdir('../data/test_structures/dna')
-        residues = []
-        for cif in dna_files:
-            structure = parser.get_structure(cif, f'../data/test_structures/dna/{cif}')
-            residues = residues + list(structure.get_residues())
+            # get all residues in all structures of the directory
+            dna_files = os.listdir(os.path.join(test_structures_dir, int_type))
+            residues = []
+            for cif in dna_files:
+                path = os.path.join(test_structures_dir, int_type, cif)
+                structure = parser.get_structure(cif, path)
+                residues = residues + list(structure.get_residues())
 
-        # remove ions/water/ligands
-        dna_residues = []
-        for res in residues:
-            if res.id[0] == ' ':
-                dna_residues.append(res)
+            # remove ions/water/ligands
+            unfiltered_residues[int_type] = []
+            for res in residues:
+                if res.id[0] == ' ':
+                    unfiltered_residues[int_type].append(res)
 
-        # test function
-        filter_residues = []
-        for res in dna_residues:
-            if is_dna(res):
-                filter_residues.append(res)
+            # test function filtering out rna and protein from dna
+            filtered_residues[int_type] = []
+            for res in residues:
+                if is_dna(res):
+                    filtered_residues[int_type].append(res)
 
-        self.assertCountEqual(dna_residues, filter_residues)
-
-    def test_is_dna_proteins(self):
-
-        parser = MMCIFParser(QUIET=True)
-
-        dna_files = os.listdir('../data/test_structures/protein')
-        residues = []
-        for cif in dna_files:
-            structure = parser.get_structure(cif, f'../data/test_structures/protein/{cif}')
-            residues = residues + list(structure.get_residues())
-
-        # remove ions/water/ligands
-        dna_residues = []
-        for res in residues:
-            if res.id[0] == ' ':
-                dna_residues.append(res)
-
-        # test function
-        filter_residues = []
-        for res in dna_residues:
-            if is_dna(res):
-                filter_residues.append(res)
-
-        self.assertEqual(len(filter_residues), 0)
-
-    def test_is_dna_rna(self):
-
-        parser = MMCIFParser(QUIET=True)
-
-        dna_files = os.listdir('../data/test_structures/rna')
-        residues = []
-        for cif in dna_files:
-            structure = parser.get_structure(cif, f'../data/test_structures/rna/{cif}')
-            for res in list(structure.get_residues()):
-                residues.append((cif, res))
-
-        # remove ions/water/ligands
-        dna_residues = []
-        for res in residues:
-            if res[1].id[0] == ' ':
-                dna_residues.append(res)
-
-        # test function
-        filter_residues = []
-        for res in dna_residues:
-            if is_dna(res[1]):
-                filter_residues.append(res)
-
-        for res in filter_residues:
-            print('structure: ', res[0], 'chain: ', res[1].get_parent().id, 'position: ', res[1].id, 'name: ', res[1].get_resname())
-        self.assertEqual(len(filter_residues), 0)
-
-
+        self.assertEqual(unfiltered_residues['dna'], filtered_residues['dna'])
+        self.assertEqual([], filtered_residues['rna'])
+        self.assertEqual([], filtered_residues['protein'])
