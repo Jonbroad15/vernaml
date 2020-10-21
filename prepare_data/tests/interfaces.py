d86640b350adbb774cf9ffce6440f82a62394172
Jonbroad15 <jonbroad15@gmail.com>
2020-10-21 17:07:04 -0400

    Add a function to find the interface annotations in the mmCIF files

--- prepare_data/tests/interfaces.py
+++ prepare_data/tests/interfaces.py
@@ -1,54 +1,59 @@
 import unittest
 from Bio.PDB import *
 from ..interfaces import *
+import os
 
 scriptdir = os.path.dirname(os.path.realpath(__file__))
-rnadir = os.path.join(scriptdir, '..', '..', '..',
-                        'data', 'rna_representative_set')
-
-ligands_file = os.path.join(scriptdir, '..', '..', '..',
+rnadir = os.path.join(scriptdir, '..', '..',
+                        'data', 'structures')
+test_structures_dir = os.path.join(scriptdir, '..', '..',
+                        'data', 'test_structures')
+ligands_file = os.path.join(scriptdir, '..', '..',
                                 'data', 'ligand_list.txt')
 ligands = []
 with open(ligands_file, 'r') as f:
     for line in f.readlines():
         ligands.append(line.strip())
 
+parser = MMCIFParser(QUIET=True)
+
 class InterfacesTestCase(unittest.TestCase):
 
 
-    def test_interface_none(self):
+    def test_get_interfaces_none(self):
 
         path = os.path.join(rnadir, '1av6.cif')
-        rna,_ = get_interfaces(path, ligands)
+        interfaces,_ = get_interfaces(path, ligands, cutoff=10)
 
-        self.assertEqual(len(rna), 0)
+        self.assertEqual(len(interfaces), 6)
 
-    def test_interface_none2(self):
+    def test_get_interfaces_SAM(self):
 
-        path ='../data/rna_structure_representative_set/1a9n.cif'
-        rna,_,_,_ = get_interfaces(path)
+        path = os.path.join(test_structures_dir, '5fk3.cif')
+        interfaces,_ = get_interfaces(path, ligands, cutoff=50)
 
-        self.assertEqual(len(rna), 0)
+        known_interfaces = find_ligand_annotations(path, ligands)
+        self.assertIn(known_interfaces, interfaces)
 
-    def test_interface_other(self):
-
-        # Should find a calcium ION here
-        path ='../data/rna_structure_representative_set/1dqf.cif'
-        _,_,other,_ = get_interfaces(path, cutoff=8)
-        binding_molecules = []
-        for line in other:
-            binding_molecules.append(line[3])
-        self.assertIn(('H_CA', 201, ' '), binding_molecules)
-
-    def test_RNA_interface(self):
-
-        path ='../data/rna_structure_representative_set/1dqf.cif'
-        rna,_,_,_ = get_interfaces(path)
-        self.assertEqual(len(rna), 18)
-
-    def test_interface_protein(self):
-
-        path ='../data/rna_structure_representative_set/1a9n.cif'
-        rna,protein,_,_ = get_interfaces(path)
-
-        self.assertIn(('1a9n', 10, 'Q'), protein)
+#    def test_get_interfaces_other(self):
+#
+#        # Should find a calcium ION here
+#        path ='../data/rna_structure_representative_set/1dqf.cif'
+#        _,_,other,_ = get_interfaces(path, cutoff=8)
+#        binding_molecules = []
+#        for line in other:
+#            binding_molecules.append(line[3])
+#        self.assertIn(('H_CA', 201, ' '), binding_molecules)
+#
+#    def test_RNA_interface(self):
+#
+#        path ='../data/rna_structure_representative_set/1dqf.cif'
+#        rna,_,_,_ = get_interfaces(path)
+#        self.assertEqual(len(rna), 18)
+#
+#    def test_interface_protein(self):
+#
+#        path ='../data/rna_structure_representative_set/1a9n.cif'
+#        rna,protein,_  = get_interfaces(path)
+#
+#        self.assertIn(('1a9n', 10, 'Q'), protein)
